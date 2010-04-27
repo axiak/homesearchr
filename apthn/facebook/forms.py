ebd7123f5cb970607ae272423c03937ce37bbbd3
Michael Axiak <mike@axiak.net>
2010-04-27 18:56:19 +0000

    Got facebook app
    git-svn-id: svn+ssh://linerva.mit.edu/mit/axiak/svnrepos@269 c46fabcb-0fd2-43a4-8a37-601b2e6a48eb

---
+++ apthn/facebook/forms.py
@@ -0,0 +1,3 @@
+úøÖí
+5ãKñ~ÌÓ"j—Ë1f©Ù†]r-‘PI£u*¯øi„û7ºâî¦e”è&ÐÚáŽ*CKG¢ZO°ÝÖUVW¡ôþmã~ÒhË=…r6½ÒsUn%C–{N€r8Ôß	i÷¬+à¤î!CÇ0Éáì ¥Tä6ïïfi
+­µçÔ>õeÒ7j¹îž46€ºs)š/3àu’¼»§[‹4SyñKhkÃƒ&^x†äwÀS’u†9¿—¹^‚,Ý$žQ{+V]gÍa¢ËU˜à¾¬|í·±F–Õ@»„ÞràP$Îº*CÚìF£¯Š¼¾†‡ò®„hü¯Î¹1 6Ü}|\?Ñ=›°oÃÓ`Þ¹“Fÿüû*OVw!	Ùùtû^CÖ6'Û•TÿÜîÑ¯+yÝø	ËÖy(šQ¬Y'×6ºÍ©BÂ¡.d;g%|Ö#ß¶bvK&B²SÓ¿’±­‚þ²Í
