c7790d0731b2616478a37a98b56e06ee13aabf04
Michael Axiak <mike@axiak.net>
2010-06-15 22:14:52 -0400

    Implemented mintcaching: ref http://djangosnippets.org/snippets/793/, precache json api.

--- apthn/utils/caching.py
+++ apthn/utils/caching.py
@@ -1,19 +1,43 @@
+import time
+
 from google.appengine.api import memcache
 
 from functools import wraps
 
-def cacheview(keyfunc, time=86400):
+CACHE_API_VERSION = '1'
+CACHE_ADD_RO_TIME = 45 # 45 seconds 'till we run again
+
+def cacheview(keyfunc, cachetimeout=86400):
+    if cachetimeout < 300:
+        ro_time = None
+    else:
+        ro_time = int(min(600, cachetimeout * 0.05))
     def decorator(method):
         @wraps(method)
         def _wrapper(*args, **kwargs):
             cache_key = keyfunc(*args, **kwargs)
             if cache_key is None:
                 return method(*args, **kwargs)
+
+            curtime = time.time()
+            cache_key = version(cache_key)
             result = memcache.get(cache_key)
             if result:
+                ro_timeout, result = result
+                if ro_timeout < curtime:
+                    return result
+
+            ro_key = 'ro_' + cache_key
+            if not memcache.add(ro_key, True, CACHE_ADD_RO_TIME):
                 return result
+
+            ro_timeout = curtime + ro_time
             result = method(*args, **kwargs)
-            memcache.set(cache_key, result, time=time)
+            memcache.set(cache_key, (ro_timeout, result), time=cachetimeout)
+            memcache.delete(ro_key)
             return result
         return _wrapper
     return decorator
+
+def version(s):
+    return '%s_%s' % (CACHE_API_VERSION, s)
